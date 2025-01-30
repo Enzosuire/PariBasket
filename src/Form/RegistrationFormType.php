adf87d33c3eb2384c4df914b352a6ddc4c941521
Enzosuire <enzosuire@yahoo.com>
2025-01-30 13:29:32 +0100

    modification affichage
    	modified:   .env
    	modified:   src/Controller/HomeController.php
    	modified:   src/Form/RegistrationFormType.php
    	modified:   templates/base.html.twig
    	modified:   templates/home/index.html.twig
    	modified:   templates/matchs/detail.html.twig
    	modified:   templates/matchs/index.html.twig

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -14,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RegistrationFormType extends AbstractType
 {
@@ -33,7 +35,11 @@ class RegistrationFormType extends AbstractType
                 'required' => false,
                 'widget' => 'single_text',
                 'attr' => ['placeholder' => 'Sélectionnez votre date de naissance'],
+                'constraints' => [
+                    new Callback([$this, 'validateAge'])
+                ],
             ])
+            
             ->add('solde', IntegerType::class, [
                 'required' => false,
                 'attr' => ['placeholder' => 'Solde initial'],
@@ -68,5 +74,20 @@ class RegistrationFormType extends AbstractType
         $resolver->setDefaults([
             'data_class' => User::class,
         ]);
+    }
+
+    public function validateAge($date, ExecutionContextInterface $context)
+    {
+        if ($date === null) {
+            return;
+        }
+
+        $age = $date->diff(new \DateTime())->y;
+
+        if ($age < 18) {
+            $context->buildViolation('Vous devez avoir au moins 18 ans pour vous inscrire.')
+                ->atPath('dateNaissance')
+                ->addViolation();
+        }
     }
 }
